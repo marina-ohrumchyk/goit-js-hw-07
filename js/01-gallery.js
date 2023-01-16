145437c4463287cb1663e07aa59a2eddebe3d428
Marina <111200416+marina-ohrumchyk@users.noreply.github.com>
2023-01-17 00:04:01 +0200

    finish commit

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -10,9 +10,9 @@ galleryConteiner.insertAdjacentHTML('beforeend', cardMarkup);
 galleryConteiner.addEventListener('click', galleryClick);
 
 function createCard(images) {
-  return images.map(({ preview, original, description }) => {
-      return;
-      `
+  return images
+    .map(({ preview, original, description }) => {
+      return `
         <div class="gallery__item">
         <a class="gallery__link" href="${original}">
           <img
@@ -28,48 +28,12 @@ function createCard(images) {
     .join('');
 }
 
-// !============
-// function galleryClick(evt){
-//   if (evt.target.nodeName !== `IMG`){
-//       return;
-//   }
-//       const instance = basicLightbox.create(`
-//       <img src="${evt.target.dataset.source}" width="800" height="600">
-//       `)
-//       instance.show();
-// }
-
-
-
-// !==============
-// function galleryClick(evn) {
-//   evn.preventDefault();
-//   const image = evn.target.classList.contains('gallery');
-//   if (!image) {
-//     return;
-//   }
-//   onModal();
-// }
-
-// function onModal() {
-//   const instance = basicLightbox.create(
-//     `
-//        <img src="${original}" width="800" height="600"
-//     `,
-//     {
-//       onShow: instance => {
-//         galleryConteiner.addEventListener('keydown', onEscapeButton);
-//       },
-//       onClose: instance => {
-//         galleryConteiner.removeEventListener('keydown', onEscapeButton);
-//       },
-//     }
-//   );
-
-//   instance.show();
-//   function onEscapeButton(evn) {
-//     if (evn.key === 'Escape') {
-//       instance.close();
-//     }
-//   }
-// }
+function galleryClick(evt) {
+  if (evt.target.nodeName !== `IMG`) {
+    return;
+  }
+  const instance = basicLightbox.create(`
+      <img src="${evt.target.dataset.source}" width="800" height="600">
+      `);
+  instance.show();
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,32 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryConteiner = document.querySelector('.gallery');
+const cardMarkup = galleryItems
+  .map(({ preview, original, description }) => {
+    return `
+        
+        <a class="gallery__item" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            alt="${description}"
+          />
+        </a>
+     
+      `;
+  })
+  .join('');
+
+galleryConteiner.insertAdjacentHTML('afterbegin', cardMarkup);
+galleryConteiner.addEventListener('click', galleryClick);
+
+function galleryClick(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+}
+
+new SimpleLightbox('.gallery a');
